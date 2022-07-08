efc23aec42292fbe396874936b5a2ee9afedd93e
Jesús Espinoza <jespinozav@isise.edu.pe>
2022-07-08 11:55:48 -0500

    Se agrego la clase Empleados

---
+++ Empleados.cs
@@ -0,0 +1,10 @@
+using System;
+
+Public Class {
+  private string _nombre;
+  private_string _apellido;
+  private_string _dni;
+  private_string _direccion;
+  private_string _correo;
+  private_string _telefono;
+}
